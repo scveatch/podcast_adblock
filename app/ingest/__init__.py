fc6df37932e59a65592769b97d3cfe10a733b81a
scveatch <scveatch@willamette.edu>
2026-03-29 23:55:26 -0700

    #6: Add base RSS support

---
+++ app/ingest/__init__.py
@@ -0,0 +1,9 @@
+"""
+File: app/ingest/__init__.py
+
+Description: Marks the module so it can be found an imported.
+
+Author: Spencer Veatch (sveatch@willamette.edu)
+
+Last Modified: 03/29/2026
+"""
